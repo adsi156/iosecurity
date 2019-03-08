761eb4d2872ff9f53dcbb7de62d2f6fa20a96a0a
yukitera <yukiterakunii@gmail.com>
2019-03-07 20:35:57 -0500

    Organizacion de menus

--- resources/views/config_componentes.blade.php
+++ resources/views/config_componentes.blade.php
@@ -1,5 +1,22 @@
 @extends('layout')
+@section('header')
+<header class="page-header">
+    <h2>Horarios</h2>
+    <div class="right-wrapper pull-right">
+        <ol class="breadcrumbs">
+            <li>
+                <a href="index.html">
+                    <i class="fa fa-home"></i>
+                </a>
+            </li>
+            <li><span>Pages</span></li>
+            <li><span>Horarios</span></li>
+        </ol>
 
+        <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+    </div>
+</header>
+@stop
 @section('content')
 <div class="row">
 	<div class="col-lg-12">
